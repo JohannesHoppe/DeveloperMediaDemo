664cffdf8abb78fbd9a3386d14083dd00e0308b0
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-08-27 14:33:08 +0200

    empty version

---
+++ 04 Web API - Teil 2/DeveloperMediaDemo - Vorher/DeveloperMediaDemo/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DeveloperMediaDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 04 Web API - Teil 2/DeveloperMediaDemo - Vorher/DeveloperMediaDemo/Scripts/knockout.bindings.js
@@ -0,0 +1,85 @@
+﻿define(['jquery', 'knockout', 'singlePage/appState', 'polyfills/iso8601', 'polyfills/datejs'],
+function ($, ko, appState) {
+
+    var colorMapping = [
+        { category: 'important', color: "red" },
+        { category: 'hobby', color: "green" },
+        { category: 'private', color: "gray" }];
+    
+    ko.bindingHandlers.choseCategoryColor = {
+        update: function (element, valueAccessor) {
+
+            var chosenColor, categories = ko.utils.unwrapObservable(valueAccessor());
+
+            // remove all already existing CSS classes
+            $.each(colorMapping, function (index, mapping) {
+                $(element).removeClass(mapping.color + "Color");
+            });
+
+            // find new class
+            $.each(colorMapping, function (index, mapping) {
+                if ($.inArray(mapping.category, categories) !== -1) {
+                    chosenColor = mapping.color;
+                    return false;
+                }
+            });
+
+            if (chosenColor) {
+                $(element).addClass(chosenColor + "Color");
+            }
+        }
+    };
+    
+    ko.bindingHandlers.changeStateOnClick = {
+        init: function (element, valueAccessor) {
+
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            $(element).click(function () {
+                appState.changeState(value.viewId, value.param);
+            });
+        }
+    };
+    
+    // uses Date.js
+    // expects a string in ISO 8601 format
+    ko.bindingHandlers.dateText = {
+        update: function (element, valueAccessor, allBindingsAccessor) {
+
+            var allBindings = allBindingsAccessor();
+            var value = ko.utils.unwrapObservable(valueAccessor());
+
+            var pattern = allBindings.datePattern || 'MM/dd/yyyy';
+            var dt = new Date(Date.parseISO(value));
+            var formatedByDatejs = dt.toString(pattern);
+
+            $(element).text(formatedByDatejs);
+        }
+    };
+
+    /* seems to be a Knockout + jQuery + Adblock Plus bug ... I don't even know where to report that issue?!
+       so i had to add event.stopPropagation();
+       */
+    ko.bindingHandlers.submit = {
+        'init': function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            if (typeof valueAccessor() != "function")
+                throw new Error("The value for a submit binding must be a function");
+            ko.utils.registerEventHandler(element, "submit", function (event) {
+                var handlerReturnValue;
+                var value = valueAccessor();
+                try { handlerReturnValue = value.call(viewModel, element); }
+                finally {
+                    if (handlerReturnValue !== true) {
+                        if (event.preventDefault) {
+                            event.preventDefault();
+
+                            // NEW - required if adblock plus is active!!
+                            event.stopPropagation();
+                        }
+                        else
+                            event.returnValue = false;
+                    }
+                }
+            });
+        }
+    };                   
+});
