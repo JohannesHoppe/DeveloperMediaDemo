267a4d7ca1e7afccdf5eb0620c271594d463ba46
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-09-19 10:10:35 +0200

    going to prepare bad practices sample

---
+++ 07 require.js - Teil 1/DeveloperMediaDemo - Vorher/DeveloperMediaDemo/AppStart/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace DeveloperMediaDemo.AppStart
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
