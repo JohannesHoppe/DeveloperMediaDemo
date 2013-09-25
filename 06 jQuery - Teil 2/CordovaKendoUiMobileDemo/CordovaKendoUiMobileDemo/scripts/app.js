438a8fabc7950d85c4d6418a555c10ae1d6cc554
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-09-25 23:23:38 +0200

    starting Kendo ui mobile demo

---
+++ 06 jQuery - Teil 2/CordovaKendoUiMobileDemo/CordovaKendoUiMobileDemo/scripts/app.js
@@ -0,0 +1,8 @@
+(function (global) {
+    var app = global.app;
+
+    document.addEventListener("deviceready", function () {
+        app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout" });
+    }, false);
+
+})(window)
