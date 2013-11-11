26e89b0f410ca840a3b1175605ea323a62deae28
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-11-11 14:19:43 +0100

    using 02 Knockout for talk

---
+++ Talks/02 Knockout - Teil 2/DeveloperMediaDemo/DeveloperMediaDemo/Controllers/NoteController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using DeveloperMediaDemo.Models;
+
+namespace DeveloperMediaDemo.Controllers
+{
+    public class NoteController : ApiController
+    {
+        public IEnumerable<Note> GetAll()
+        {
+            return NoteRepository.ReadAll();
+        }
+
+        public Note Get(int id)
+        {
+            return NoteRepository.Read(id);
+        }
+
+        public int Post()
+        {
+            var newNote = new Note();
+            NoteRepository.Create(newNote);
+            return newNote.Id;
+        }
+
+        public void Put(Note note)
+        {
+            NoteRepository.Update(note);
+        }
+
+        public void Delete(int id)
+        {
+            NoteRepository.Delete(id);
+        }
+    }
+}
